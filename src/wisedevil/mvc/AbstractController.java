2d056760d7775bfce8ac2c71098324e6379cdf4f
Alfredo Mungo <chimeranet89@gmail.com>
2014-06-30 22:04:19 +0200

    Improved documentation, added license, general structure refactoring

--- src/wisedevil/mvc/AbstractController.java
+++ src/wisedevil/mvc/AbstractController.java
@@ -1,26 +1,51 @@
+/*
+ * Wisedevil's MVC: MVC library for the Java platform
+ *    Copyright (C) 2014  Alfredo Mungo
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package wisedevil.mvc;
+
+import java.util.Observer;
+import java.util.Observable;
 
 /**
  * This class represents a controller object.
+ *
+ * @param <M> The model type which is going to be controlled
  */
-public abstract class AbstractController {
+public abstract class AbstractController<M extends AbstractModel> implements Observer {
 	/**
 	 * The associated model.
 	 */
-	private final AbstractModel model;
+	private M model;
 	
 	/**
 	 * The associated view.
 	 */
-	private AbstractView view;
+	private AbstractView<M> view;
 	
 	/**
 	 * Initializes a new instance of this class.
 	 *
 	 * @param model The associated model object.
 	 */
-	protected AbstractController(AbstractModel model) {
+	protected AbstractController(M model) {
 		this.model = model;
+		
+		// Register against the model for observation
+		setModel(model);
 	}
 	
 	/**
@@ -28,14 +53,29 @@ public abstract class AbstractController {
 	 *
 	 * @return The associated view object
 	 */
-	public AbstractView getView() { return view; }
+	public AbstractView<M> getView() { return view; }
 	
 	/**
 	 * Returns the associated model object.
 	 *
 	 * @return The associated model object
 	 */
-	public AbstractModel getModel() { return model; }
+	public M getModel() { return model; }
+	
+	/**
+	 * Sets a new model.
+	 *
+	 * @param model The new model
+	 */
+	 public void setModel(M model) {
+		if(this.model != null)
+			this.model.deleteObserver(this);
+		
+		this.model = model;
+		
+		if(model != null)
+			model.addObserver(this);
+	 }
 	
 	/**
 	 * Returns whether the controller has an associated view or not.
@@ -49,6 +89,30 @@ public abstract class AbstractController {
 	 *
 	 * @param v The view to associate with this controller
 	 */
-	void setView(AbstractView v) { view = v; }
+	void setView(AbstractView<M> v) { view = v; }
+	
+	/**
+	 * This method is called whenever a model object is updated.
+	 *
+	 * The general contract for this method is to call the <code>update(Object)</code> method whenever an object (of the class of the associated controller's model) is updated.
+	 *
+	 * @see java.util.Observer#update(java.util.Observable, Object)
+	 * @see #update(Object)
+	 */
+	public void update(Observable o, Object arg) {
+		Class<? extends AbstractModel> cls = getModel().getClass();
+		
+		if(cls.isInstance(o))
+			update(arg);
+	}
+	
+	/**
+	 * This method is called whenever an instance of the model is updated.
+	 *
+	 * @param arg The updated object in the model instance
+	 *
+	 * @see #update(java.util.Observable, Object)
+	 */
+	protected abstract void update(Object arg);
 }
 
--- src/wisedevil/mvc/AbstractView.java
+++ src/wisedevil/mvc/AbstractView.java
@@ -1,16 +1,32 @@
+/*
+ * Wisedevil's MVC: MVC library for the Java platform
+ *    Copyright (C) 2014  Alfredo Mungo
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package wisedevil.mvc;
-
-import java.util.Observer;
-import java.util.Observable;
 
 /**
  * This class represents a view object.
+ *
+ * @param <M> The model type which is going to be viewed
  */
-public abstract class AbstractView implements Observer, Viewable {
+public abstract class AbstractView<M extends AbstractModel> implements Viewable {
 	/**
 	 * The associated controller object.
 	 */
-	private final AbstractController controller;
+	private final AbstractController<M> controller;
 	
 	/**
 	 * A value representing the visibility of the object (defaults to <code>false</code>).
@@ -26,7 +42,7 @@ public abstract class AbstractView implements Observer, Viewable {
 	 *
 	 * @throws NullPointerException if <code>c</code> is null
 	 */
-	protected AbstractView(AbstractController c) throws NullPointerException {
+	protected AbstractView(AbstractController<M> c) {
 		
 		if(c != null) {
 			// Set the controller
@@ -34,9 +50,6 @@ public abstract class AbstractView implements Observer, Viewable {
 			
 			// Register against the controller
 			c.setView(this);
-			
-			// Register against the model for observation
-			c.getModel().addObserver(this);
 		} else throw new NullPointerException(); // Controller cannot be null
 	}
 	
@@ -45,31 +58,7 @@ public abstract class AbstractView implements Observer, Viewable {
 	 *
 	 * @return The associated controller object
 	 */
-	public AbstractController getController() { return controller; }
-	
-	/**
-	 * This method is called whenever a model object is updated.
-	 *
-	 * The general contract for this method is to call the <code>update(Object)</code> method whenever an object (of the class of the associated controller's model) is updated.
-	 *
-	 * @see java.util.Observer#update(java.util.Observable, Object)
-	 * @see #update(Object)
-	 */
-	public void update(Observable o, Object arg) {
-		Class<? extends AbstractModel> cls = getController().getModel().getClass();
-		
-		if(cls.isInstance(o))
-			update(arg);
-	}
-	
-	/**
-	 * This method is called whenever an instance of the model is updated.
-	 *
-	 * @param arg The updated model instance
-	 *
-	 * @see #update(java.util.Observable, Object)
-	 */
-	protected abstract void update(Object arg);
+	public AbstractController<M> getController() { return controller; }
 	
 	/**
 	 * Sets the visibility of the view.
